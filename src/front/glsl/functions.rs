8c822a000e22f8194f328cfa4438438f43368182
Pelle Johnsen <pelle.johnsen@gmail.com>
2021-01-12 08:08:24 +0000

    [glsl-in] Implement function call

--- src/front/glsl/functions.rs
+++ src/front/glsl/functions.rs
@@ -108,7 +108,25 @@ impl Program {
                                 .collect(),
                         })
                     }
-                    _ => Err(ErrorKind::NotImplemented("Function call")),
+                    func_name => {
+                        let function = *self
+                            .lookup_function
+                            .get(func_name)
+                            .ok_or(ErrorKind::SemanticError("Unknown function"))?;
+                        Ok(ExpressionRule {
+                            expression: self.context.expressions.append(Expression::Call {
+                                function,
+                                arguments: fc.args.iter().map(|a| a.expression).collect(),
+                            }),
+                            sampler: None,
+                            statements: fc
+                                .args
+                                .into_iter()
+                                .map(|a| a.statements)
+                                .flatten()
+                                .collect(),
+                        })
+                    }
                 }
             }
         }
