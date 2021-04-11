fbb84e70d2d33969371157799b8dfb42971d32a1
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-04-11 19:01:54 +0200

    Formatting

--- src/back/msl/writer.rs
+++ src/back/msl/writer.rs
@@ -676,8 +676,9 @@ impl<W: Write> Writer<W> {
                 write!(self.out, "{}", name)?;
             }
             crate::Expression::Load { pointer } => {
-                // We don't do any dereferencing with `*` here as pointer arguments are done
-                // by `&` references and not `*` pointers, which do not need to be dereferenced.
+                // We don't do any dereferencing with `*` here as pointer arguments to functions
+                // are done by `&` references and not `*` pointers, which do not need to be
+                // dereferenced.
                 self.put_expression(pointer, context, is_scoped)?;
             }
             crate::Expression::ImageSample {
