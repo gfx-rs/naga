18b92a67af004e8b1f5336b0fec348627acf8836
Lachlan Sneff <lachlan.sneff@gmail.com>
2021-04-30 22:31:18 -0400

    Update stack size for msl backend

--- src/back/msl/writer.rs
+++ src/back/msl/writer.rs
@@ -2342,8 +2342,8 @@ fn test_stack_size() {
         }
         let stack_size = addresses.end - addresses.start;
         // check the size (in debug only)
-        // last observed macOS value: 21760
-        if stack_size < 21000 || stack_size > 23000 {
+        // last observed macOS value: 23040
+        if stack_size < 21000 || stack_size > 25000 {
             panic!("`put_expression` stack size {} has changed!", stack_size);
         }
     }
