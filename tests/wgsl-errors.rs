cbef4b755a2825f7466f39072fbeaf827b5c23ea
Hans Christian Schmitz <hcsch@hcsch.eu>
2021-08-22 15:22:57 +0200

    Update error message tests

--- tests/wgsl-errors.rs
+++ tests/wgsl-errors.rs
@@ -49,13 +49,11 @@ fn invalid_integer() {
 fn invalid_float() {
     check(
         "let scale: f32 = 1.1.;",
-        r###"error: expected floating-point literal, found `1.1.`
-  ┌─ wgsl:1:18
+        r###"error: expected ';', found '.'
+  ┌─ wgsl:1:21
   │
 1 │ let scale: f32 = 1.1.;
-  │                  ^^^^ expected floating-point literal
-  │
-  = note: invalid float literal
+  │                     ^ expected ';'
 
 "###,
     );
@@ -102,11 +100,11 @@ fn negative_index() {
                 return a[-1];
             }
         "#,
-        r#"error: expected non-negative integer constant expression, found `-1`
+        r#"error: expected unsigned integer constant expression, found `-1`
   ┌─ wgsl:4:26
   │
 4 │                 return a[-1];
-  │                          ^^ expected non-negative integer
+  │                          ^^ expected unsigned integer
 
 "#,
     );
