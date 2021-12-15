7c851b65400bfaa95bac3ae41b1bbb4480a79cb2
João Capucho <jcapucho7@gmail.com>
2021-12-15 21:35:19 +0000

    spv-in: Allow branch weights in OpBranchConditional

--- src/front/spv/mod.rs
+++ src/front/spv/mod.rs
@@ -2814,6 +2814,11 @@ impl<I: Iterator<Item = u32>> Parser<I> {
                         reject,
                     });
 
+                    // Consume branch weights
+                    for _ in 4..inst.wc {
+                        let _ = self.next()?;
+                    }
+
                     return Ok(());
                 }
                 Op::Switch => {
