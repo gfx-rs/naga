7352f84bf18036fad94931db64e241effec0dba4
Franz <francesco.cattoglio@gmail.com>
2022-02-02 13:04:04 +0100

    Committed all test output changes

--- tests/out/glsl/functions.main.Compute.glsl
+++ tests/out/glsl/functions.main.Compute.glsl
@@ -17,13 +17,13 @@ vec2 test_fma() {
 void test_integer_dot_product() {
     ivec2 a_2_ = ivec2(1);
     ivec2 b_2_ = ivec2(1);
-    int c_2_ = (a_2_.x * b_2_.x + a_2_.y * b_2_.y);
+    int c_2_ = ( + a_2_.x * b_2_.x + a_2_.y * b_2_.y);
     uvec3 a_3_ = uvec3(1u);
     uvec3 b_3_ = uvec3(1u);
-    uint c_3_ = (a_3_.x * b_3_.x + a_3_.y * b_3_.y + a_3_.z * b_3_.z);
+    uint c_3_ = ( + a_3_.x * b_3_.x + a_3_.y * b_3_.y + a_3_.z * b_3_.z);
     ivec4 _e11 = ivec4(4);
     ivec4 _e13 = ivec4(2);
-    int c_4_ = (_e11.x * _e13.x + _e11.y * _e13.y + _e11.z * _e13.z + _e11.w * _e13.w);
+    int c_4_ = ( + _e11.x * _e13.x + _e11.y * _e13.y + _e11.z * _e13.z + _e11.w * _e13.w);
 }
 
 void main() {
