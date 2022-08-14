c9791aa2f35826383735b230532e2e72b60702e7
João Capucho <jcapucho7@gmail.com>
2022-08-14 18:07:22 +0100

    hlsl: Test push constant and vertex builtins

--- tests/out/hlsl/push-constants.hlsl
+++ tests/out/hlsl/push-constants.hlsl
@@ -1,3 +1,9 @@
+struct NagaConstants {
+    int base_vertex;
+    int base_instance;
+    uint other;
+};
+ConstantBuffer<NagaConstants> _NagaConstants: register(b0, space1);
 
 struct PushConstants {
     float multiplier;
@@ -12,6 +18,12 @@ ConstantBuffer<PushConstants> pc: register(b0);
 struct FragmentInput_main {
     float4 color : LOC0;
 };
+
+float4 vert_main(float2 pos : LOC0, uint vi : SV_VertexID) : SV_Position
+{
+    float _expr5 = pc.multiplier;
+    return float4(((float((_NagaConstants.base_vertex + vi)) * _expr5) * pos), 0.0, 1.0);
+}
 
 float4 main(FragmentInput_main fragmentinput_main) : SV_Target0
 {
