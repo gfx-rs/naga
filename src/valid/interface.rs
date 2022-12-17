66b758ee087898098537cf461382ef5d090bd82d
Jim Blandy <jimb@red-bean.com>
2022-12-16 16:16:07 -0800

    Fix warnings when building tests without validation.

--- src/valid/interface.rs
+++ src/valid/interface.rs
@@ -102,12 +102,14 @@ fn storage_usage(access: crate::StorageAccess) -> GlobalUse {
 struct VaryingContext<'a> {
     stage: crate::ShaderStage,
     output: bool,
-    flags: super::ValidationFlags,
     types: &'a UniqueArena<crate::Type>,
     type_info: &'a Vec<super::r#type::TypeInfo>,
     location_mask: &'a mut BitSet,
     built_ins: &'a mut crate::FastHashSet<crate::BuiltIn>,
     capabilities: Capabilities,
+
+    #[cfg(feature = "validate")]
+    flags: super::ValidationFlags,
 }
 
 impl VaryingContext<'_> {
@@ -339,8 +341,7 @@ impl VaryingContext<'_> {
                         for (index, member) in members.iter().enumerate() {
                             let span_context = self.types.get_span_context(ty);
                             match member.binding {
-                                None =>
-                                {
+                                None => {
                                     #[cfg(feature = "validate")]
                                     if self.flags.contains(super::ValidationFlags::BINDINGS) {
                                         return Err(VaryingError::MemberMissingBinding(
@@ -348,6 +349,8 @@ impl VaryingContext<'_> {
                                         )
                                         .with_span_context(span_context));
                                     }
+                                    #[cfg(not(feature = "validate"))]
+                                    let _ = index;
                                 }
                                 // TODO: shouldn't this be validate?
                                 Some(ref binding) => self
@@ -515,12 +518,14 @@ impl super::Validator {
             let mut ctx = VaryingContext {
                 stage: ep.stage,
                 output: false,
-                flags: self.flags,
                 types: &module.types,
                 type_info: &self.types,
                 location_mask: &mut self.location_mask,
                 built_ins: &mut argument_built_ins,
                 capabilities: self.capabilities,
+
+                #[cfg(feature = "validate")]
+                flags: self.flags,
             };
             ctx.validate(fa.ty, fa.binding.as_ref())
                 .map_err_inner(|e| EntryPointError::Argument(index as u32, e).with_span())?;
@@ -532,12 +537,14 @@ impl super::Validator {
             let mut ctx = VaryingContext {
                 stage: ep.stage,
                 output: true,
-                flags: self.flags,
                 types: &module.types,
                 type_info: &self.types,
                 location_mask: &mut self.location_mask,
                 built_ins: &mut result_built_ins,
                 capabilities: self.capabilities,
+
+                #[cfg(feature = "validate")]
+                flags: self.flags,
             };
             ctx.validate(fr.ty, fr.binding.as_ref())
                 .map_err_inner(|e| EntryPointError::Result(e).with_span())?;
