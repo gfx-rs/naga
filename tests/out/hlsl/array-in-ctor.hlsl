e8032b6d6745ccb3e61c4e545d7658de21fb4c31
Erich Gubler <erichdongubler@gmail.com>
2023-03-15 16:48:55 -0400

    fix(hlsl): emit constructor functions for arrays

--- tests/out/hlsl/array-in-ctor.hlsl
+++ tests/out/hlsl/array-in-ctor.hlsl
@@ -5,6 +5,12 @@ struct Ah {
 
 ByteAddressBuffer ah : register(t0);
 
+typedef float ret_Constructarray2_float_[2];
+ret_Constructarray2_float_ Constructarray2_float_(float arg0, float arg1) {
+    float ret[2] = { arg0, arg1 };
+    return ret;
+}
+
 Ah ConstructAh(float arg0[2]) {
     Ah ret = (Ah)0;
     ret.inner = arg0;
@@ -14,5 +20,5 @@ Ah ConstructAh(float arg0[2]) {
 [numthreads(1, 1, 1)]
 void cs_main()
 {
-    Ah unnamed = ConstructAh({asfloat(ah.Load(0+0)), asfloat(ah.Load(0+4))});
+    Ah unnamed = ConstructAh(Constructarray2_float_(asfloat(ah.Load(0+0)), asfloat(ah.Load(0+4))));
 }
