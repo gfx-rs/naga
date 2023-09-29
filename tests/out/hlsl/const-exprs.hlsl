436c2cd4269c28869cae67ef136c73b0f3bf17c8
teoxoy <28601907+teoxoy@users.noreply.github.com>
2023-09-29 19:20:50 +0200

    [wgsl-in] don't treat `let` declarations as `const` declarations

--- tests/out/hlsl/const-exprs.hlsl
+++ tests/out/hlsl/const-exprs.hlsl
@@ -8,16 +8,12 @@ RWByteAddressBuffer out2_ : register(u1);
 
 void swizzle_of_compose()
 {
-    int2 a = int2(1, 2);
-    int2 b = int2(3, 4);
     out_.Store4(0, asuint(int4(4, 3, 2, 1)));
     return;
 }
 
 void index_of_compose()
 {
-    int2 a_1 = int2(1, 2);
-    int2 b_1 = int2(3, 4);
     int _expr8 = asint(out2_.Load(0));
     out2_.Store(0, asuint((_expr8 + 2)));
     return;
